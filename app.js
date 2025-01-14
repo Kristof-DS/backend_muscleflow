3ba291b8a0d50540dec64783213fdb1fb9a9481d
Kristof-DS <kristof.ds@gmail.com>
2025-01-14 11:14:54 +0100

    set up the listener

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import 'dotenv/config'
 import sql from './src/database.js'
+import express from 'express'
 
 
+const app = express()
 
+app.get('/test', (req, res) => {
+    res.send('Hello Jhonsons')
+})
+
+app.listen(process.env.PORT || 3000)
