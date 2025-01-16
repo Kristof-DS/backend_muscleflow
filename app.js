a4b992d93bd1a936071ee6635c9e8673ae9c75ee
Kristof-DS <kristof.ds@gmail.com>
2025-01-16 11:59:24 +0100

    Changed the CORS options

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+    origin: ['http://localhost:5173']
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 
