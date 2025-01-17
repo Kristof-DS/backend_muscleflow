c4f60cfd8b87861c6d946262f447183895d91e76
Kristof-DS <kristof.ds@gmail.com>
2025-01-17 10:36:45 +0100

    Modified the database variables

--- src/database.js
+++ src/database.js
@@ -1,4 +1,4 @@
-import postgres from 'postgres'
+/*import postgres from 'postgres'
 
 const sql = postgres({
     user: process.env.DB_USERNAME,
@@ -12,3 +12,18 @@ const sql = postgres({
 })
 
 export default sql
+
+*/
+
+import postgres from 'postgres';
+
+const connectionString = process.env.DATABASE_URL || `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DATABASE}`;
+
+const sql = postgres(connectionString, {
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // Enable SSL for Heroku
+    max: 20,
+    idle_timeout: 30000,
+    connect_timeout: 2000,
+});
+
+export default sql;
