7ceea3bf9aba0cefaddc1b8d4045c3319030aba6
Kristof-DS <kristof.ds@gmail.com>
2025-01-27 16:41:05 +0100

    Created the create-workout functionality

--- src/User/getDashboardDetails.js
+++ src/User/getDashboardDetails.js
@@ -1,34 +1,39 @@
 import sql from '../database.js'
 
-const getDashboardDetails = async(req, res) => {
+const getDashboardDetails = async (req, res) => {
     const id_user = req.id_user
+    try {
+        const userDetails = await sql`SELECT username, weight, height FROM users WHERE id = ${id_user}`
 
-    const userDetails = await sql `SELECT username, weight, height FROM users WHERE id = ${id_user}`
-    
-    const savedWorkouts = await sql `SELECT * FROM workouts WHERE id_user = ${id_user}`
-   
-    const SavedWorkoutExercises = []
+        const savedWorkouts = await sql`SELECT * FROM workouts WHERE id_user = ${id_user}`
 
-    for (let x = 0; x < savedWorkouts.length; x++) {
-        const exercises = await sql `SELECT * FROM exercises WHERE id_workout = ${savedWorkouts[x].id}`
-        
-        exercises.forEach((exercise) => SavedWorkoutExercises.push(exercise))
+        const SavedWorkoutExercises = []
+
+        for (let x = 0; x < savedWorkouts.length; x++) {
+            const exercises = await sql`SELECT * FROM exercises WHERE id_workout = ${savedWorkouts[x].id}`
+
+            exercises.forEach((exercise) => SavedWorkoutExercises.push(exercise))
+        }
+
+
+
+        const workoutsWithExercises = savedWorkouts.map(workout => {
+
+            const exercises = SavedWorkoutExercises.filter(exercise => exercise.id_workout === workout.id);
+
+            return {
+                ...workout,
+                exercises
+            };
+        });
+
+        return res.status(200).json({ userDetails, savedWorkouts: workoutsWithExercises })
+    }
+    catch (err) {
+        res.status(500).send('Internal server error. Failed to fetch data')
     }
 
-
-
-  const workoutsWithExercises = savedWorkouts.map(workout => {
-   
-    const exercises = SavedWorkoutExercises.filter(exercise => exercise.id_workout === workout.id);
-
-    return {
-      ...workout,
-      exercises
-    };
-  });
-
-return res.status(200).json({userDetails, savedWorkouts: workoutsWithExercises})
-   
 }
+
 
 export default getDashboardDetails
--- src/User/router.js
+++ src/User/router.js
@@ -2,11 +2,14 @@ import sql from '../database.js'
 import express from 'express'
 import updateUserDetails from './updateUserDetails.js'
 import getDashboardDetails from './getDashboardDetails.js'
-
+import createWorkout from './createWorkout.js'
 
 const router = express.Router()
 
 router.put('/:id_user/details', updateUserDetails)
 router.get('/:id_user/dashboard', getDashboardDetails)
+router.post('/:id_user/workout', createWorkout)
+//router.put('/:id_user/workout', editWorkout)
+//router.delete('/:id_user/workout', deleteWorkout)
 
 export default router
